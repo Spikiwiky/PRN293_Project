8f9e99cc0d437c0bb8a730d485d5d5c0239d7aab
Spikiwiky <dragon.spiki@gmail.com>
2025-06-02 18:39:53 +0700

    Add Register Feature

--- PRN293_SourceCode/EcommerceBackend.BusinessObject/Abstract/AuthAbstract/IAuthService.cs
+++ PRN293_SourceCode/EcommerceBackend.BusinessObject/Abstract/AuthAbstract/IAuthService.cs
@@ -1,4 +1,5 @@
 ﻿using EcommerceBackend.BusinessObject.dtos.AuthDto;
+using EcommerceBackend.DataAccess.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,6 @@ namespace EcommerceBackend.BusinessObject.Abstract.AuthAbstract
         public string GenerateJwtToken(UserDto user);
         bool IsValidEmail(string email);
         UserDto? ValidateUser(LoginRequestDto loginRequest);
+        UserDto RegisterUser(string email, string password, string userName, string? phone, DateTime? dateOfBirth, string? address);
     }
 }
