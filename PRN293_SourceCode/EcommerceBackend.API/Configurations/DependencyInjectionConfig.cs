901fcb32c6ab6ac7fce74bf2ad117c34060eea97
Spikiwiky <dragon.spiki@gmail.com>
2025-07-01 09:53:43 +0700

    Add Cart Feature

--- PRN293_SourceCode/EcommerceBackend.API/Configurations/DependencyInjectionConfig.cs
+++ PRN293_SourceCode/EcommerceBackend.API/Configurations/DependencyInjectionConfig.cs
@@ -12,6 +12,10 @@ using EcommerceBackend.DataAccess.Repository;
 using EcommerceBackend.BusinessObject.Services;
 
 using EcommerceBackend.DataAccess.Abstract;
+using EcommerceBackend.BusinessObject.Abstract.OrderAbstract;
+using EcommerceBackend.BusinessObject.Services.OrderService;
+using EcommerceBackend.DataAccess.Abstract.OrderAbstract;
+using EcommerceBackend.DataAccess.Repository.OrderRepository;
 
 
 namespace EcommerceBackend.API.Configurations
@@ -38,6 +42,14 @@ namespace EcommerceBackend.API.Configurations
             //Register for auth
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IAuthRepository, AuthRepository>();
+
+            //Register for order
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+
+
+
 
             // Register other services as needed
             //services.AddScoped<IUserService, UserService>();
