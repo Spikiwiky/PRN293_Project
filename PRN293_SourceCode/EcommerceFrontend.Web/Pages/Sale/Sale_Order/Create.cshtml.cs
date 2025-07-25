7dce1cc766f15d1075b384e82b5456f1bf9f3eaf
GaGionCay <minhtrihoangngoc@gmail.com>
2025-07-25 22:30:01 +0700

    321

---
+++ PRN293_SourceCode/EcommerceFrontend.Web/Pages/Sale/Sale_Order/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcommerceFrontend.Web.Pages.Sale.Sale_Order
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
