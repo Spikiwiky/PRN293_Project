a8bfd1abe00dc66655320fbceb45fe76dbaaf554
GaGionCay <minhtrihoangngoc@gmail.com>
2025-07-25 22:39:10 +0700

    321

---
+++ PRN293_SourceCode/EcommerceFrontend.Web/Pages/Sale/Sale_User/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcommerceFrontend.Web.Pages.Sale.Sale_User
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
