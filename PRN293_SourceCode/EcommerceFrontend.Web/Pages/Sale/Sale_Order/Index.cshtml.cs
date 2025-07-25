2cbc6f061b5b0f8017292c498acdf7b0f7b98174
GaGionCay <minhtrihoangngoc@gmail.com>
2025-07-25 22:35:14 +0700

    321

--- PRN293_SourceCode/EcommerceFrontend.Web/Pages/Sale/Sale_Order/Index.cshtml.cs
+++ PRN293_SourceCode/EcommerceFrontend.Web/Pages/Sale/Sale_Order/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EcommerceFrontend.Web.Pages.Sale
+namespace EcommerceFrontend.Web.Pages.Sale.Sale_Order
 {
     public class IndexModel : PageModel
     {
