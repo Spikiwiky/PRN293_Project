4b49bce9d55f46bd9f659e7d15728b686a6284d8
GaGionCay <minhtrihoangngoc@gmail.com>
2025-07-25 21:11:52 +0700

    321

--- PRN293_SourceCode/EcommerceBackend.BusinessObject/Services/SaleService/UserService/IUserService.cs
+++ PRN293_SourceCode/EcommerceBackend.BusinessObject/Services/SaleService/UserService/IUserService.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using EcommerceBackend.DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EcommerceBackend.BusinessObject.dtos.UserDto;
+using EcommerceBackend.BusinessObject.dtos.SaleDto;
 namespace EcommerceBackend.BusinessObject.Services.SaleService.UserService
 {
-    internal class IUserService
+    public interface ISaleUserService
     {
+        IEnumerable<User> GetAll();
+        User? GetById(int id);
+        void Add(UserCreateDto user);
+        void Update(UserDto user);
+        void Delete(int id);
     }
 }
