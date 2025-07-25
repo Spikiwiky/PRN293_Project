2512f1e84801f61fa3c34698830f372fd65c5fde
GaGionCay <minhtrihoangngoc@gmail.com>
2025-07-25 21:05:17 +0700

    orderservice

---
+++ PRN293_SourceCode/EcommerceBackend.BusinessObject/Services/SaleService/OrderService/ISaleOrderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceBackend.BusinessObject.Services.SaleService.OrderService
+{
+    internal class ISaleOrderService
+    {
+    }
+}
